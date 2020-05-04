56f128cc5437740a46f79d96b67a65e9ff7af651
winecos <dev@winecos.com>
2020-05-04 22:00:20 +0800

    add css of the website

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,27 +4,37 @@ module.exports = {
   lastUpdated: 'Last Updated',
   themeConfig: {
     logo: "/logo.png",
+    docsRepo: 'kucodeai/docs',
+    docsDir: 'docs',
+    editLinks: true,
     sidebarDepth: 2,
     nav: [
       { text: '官网', link: 'https://kucodeai.com' },
-      { text: '课程', link: 'https://courses.kucodeai.com' },
+      { text: '课程', link: 'https://kucodeai.com/#/courses' },
       { text: 'GitHub', link: 'https://github.com/kucodeai/' },
       {
         text: 'Languages',
         ariaLabel: 'Language Menu',
         items: [
-          {text: '简体中文',link:'/language/Chinese/'},
-          {text: 'English',link: '/language/English'}
+          { text: '简体中文', link: '/language/Chinese/' },
+          { text: 'English', link: '/language/English' }
         ]
       }
     ],
     sidebar: {
       '/': [
         {
-          title: 'Introduction',
+          title: '概况',
           collapsable: false,
           children: [
             '/'
+          ]
+        },
+        {
+          title:'计算机视觉-OpenCV',
+          collapsable:false,
+          children:[
+            '/opencv-install'
           ]
         }
       ]
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,9 @@
-# About KucodeAI
+# 关于酷编程AI知识库
 
-KucodeAI is a delightfully simple discussion platform for your website. It's fast and easy to use, with all the features you need to run a successful community.
+酷编程人工智能知识库汇集所有学习文档资源，包括课程介绍、人工智能相关知识库积累等
 
 This user guide will help you to set up your own forum using KucodeAI and teach you the basics of managing your community.
 
-## Goals
+## 目标
 
-KucodeAI is the combined successor of [esoTalk](https://github.com/esotalk/esoTalk) and [FluxBB](https://fluxbb.org). It is designed to be
+为学习者提供有帮助的资源和知识沉淀积累
