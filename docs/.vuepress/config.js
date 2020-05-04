c371615bc14aa4ce054726be434e8e1455fc13e5
winecos <dev@winecos.com>
2020-05-04 16:59:35 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,112 @@
+module.exports = {
+  title: 'Flarum Documentation',
+  base: '/docs/',
+
+  themeConfig: {
+     algolia: {
+      apiKey: '8f760cdb850b1e696b72329eed96b01b',
+      indexName: 'flarum'
+    },
+    lastUpdated: 'Last Updated',
+    docsRepo: 'flarum/docs',
+    docsDir: 'docs',
+    editLinks: true,
+    nav: [
+      { text: 'Guide', link: '/' },
+      { text: 'Extend', link: '/extend/' },
+      { text: 'API Reference', link: 'https://api.flarum.dev/' },
+      {
+        text: 'Flarum',
+        items: [
+          { text: 'Home', link: 'https://flarum.org/' },
+          { text: 'Community', link: 'https://discuss.flarum.org/' },
+          { text: 'GitHub', link: 'https://github.com/flarum/flarum/' },
+        ]
+      }
+    ],
+    sidebar: {
+      '/extend/': [
+        {
+          title: 'Main Concepts',
+          collapsable: false,
+          children: [
+            '/extend/',
+            '/extend/start',
+            '/extend/frontend',
+            '/extend/routes',
+            '/extend/data',
+            '/extend/distribution',
+            '/extend/update-b8',
+            '/extend/update-b10',
+            '/extend/update-b12',
+          ]
+        },
+        {
+          title: 'Advanced Guides',
+          collapsable: false,
+          children: [
+            '/extend/authorization',
+            '/extend/console',
+            '/extend/formatting',
+            '/extend/i18n',
+            '/extend/middleware',
+            '/extend/notifications',
+            '/extend/permissions',
+            '/extend/post-types',
+            '/extend/search',
+            '/extend/settings',
+            '/extend/testing',
+          ]
+        },
+        // {
+        //   title: 'Themes',
+        //   collapsable: false,
+        //   children: [
+        //     '/extend/theme',
+        //   ]
+        // },
+        // {
+        //   title: 'Localization',
+        //   collapsable: false,
+        //   children: [
+        //   ]
+        // }
+      ],
+      '/': [
+        {
+          title: 'Introduction',
+          collapsable: false,
+          children: [
+            '/',
+            '/code-of-conduct',
+            '/releases',
+            '/contributing',
+            '/bugs',
+            '/faq'
+          ]
+        },
+        {
+          title: 'Setting Up',
+          collapsable: false,
+          children: [
+            '/install',
+            '/update',
+            '/troubleshoot'
+          ]
+        },
+        {
+          title: 'Customization',
+          collapsable: false,
+          children: [
+            '/config',
+            '/appearance',
+            '/extensions',
+            '/languages',
+            '/tags',
+            '/permissions'
+          ]
+        }
+      ]
+    }
+  }
+};
